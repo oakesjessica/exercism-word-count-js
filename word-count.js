4f303bc73c9b263633605e6860294251fc67c46d
Jessica Oakes <jessica@oakesdev.com>
2022-03-06 13:33:11 -0600

    Replace single quotes so we can quote contractions with apostrophe prefix

--- word-count.js
+++ word-count.js
@@ -1,12 +1,16 @@
 /**
  * Currently only consider ASCII letters and digits
- * /\b[A-Za-z+'A-Za-z]+\b/ => capture any character between word boundaries
- *  Matches simple words, possessive and contractions *but*
- *    currently drops the apostrophe with contracts like 'ow, and 'twas. It matches "ow" and "twas"
+ *  Pattern does not match hyphenated words as we're only consider simple words and contractions
+ * /[A-Za-z']+\b/ => capture any character between word boundaries
+ *  Matches simple words, possessive and contractions
+ *  IMPORTANT: This pattern assumes there are no single quoted words
+ *    TODO: find a REGEX-only way to handle both single-quoted words and contracted words with a leading apostrophe
+ *      without false positive results, e.g. 'large, 'here is a bad match
  * /\b[0-9]+\b/ => capture digits only, exclude alphanumeric patterns, e.g. testing123, 123testing, test123test
- *
+ * Pattern does not include
 **/
-const WORD_COUNT_REGEX = /\b[A-Za-z'A-Za-z]+\b|\b[0-9]+\b/gm;
+const WORD_COUNT_REGEX = /[A-Za-z']+\b|\b[0-9]+\b/gm;
+const SINGLE_QUOTED_WORDS_REGEX = /\s+?'([A-Za-z]+)'/gm;
 
 export const countWords = (str) => {
   if ( ! str || str.length === 0) {
@@ -14,6 +18,9 @@ export const countWords = (str) => {
   }
 
   const results = {};
+  // Replace any quoted words, e.g. 'letters', 'hey, this is a quote'
+  // This way, we can capture contractions with leading apostrophes, e.g. 'twas, 'ow
+  str = str.replace(SINGLE_QUOTED_WORDS_REGEX, " $1 ");
   str = str.toLowerCase();
 
   str.match(WORD_COUNT_REGEX).forEach(s => {
